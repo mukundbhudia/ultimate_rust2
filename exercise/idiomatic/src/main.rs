0a26a9ee4e38d0220ac1dfc454ba90fc12e1835c
Mukund Bhudia <code@mukund.uk>
2024-08-09 11:39:47 +0100

    chore: spacing

--- exercise/idiomatic/src/main.rs
+++ exercise/idiomatic/src/main.rs
@@ -9,8 +9,10 @@
 // Challenge: Clippy doesn't find *everything*. What else would you change to make this code better?
 
 const pi: f32 = 3.14159265358979323846;
+
 fn count_to_5() -> i32 {
     let mut foo = 0;
+
     loop {
         if foo > pi as i32 {
             if foo > 5 {
@@ -19,14 +21,17 @@ fn count_to_5() -> i32 {
         }
         foo = foo + 1;
     }
+
     return 5;
 }
 fn main() {
     println!("I can count to {}", count_to_5());
 }
+
 #[cfg(test)]
 mod test {
     use super::*;
+
     #[test]
     fn test_counting() {
         assert_eq!(count_to_5() == 5, true);
